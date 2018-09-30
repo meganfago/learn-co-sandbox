6bb85402b474ad2bbff244fafdd46d15abf3ff22
benefic-dispatch-1249 <meganlaynefago@gmail.com>
2018-09-30 20:04:13 +0000

    ok

---
+++ conference_badges.rb
@@ -0,0 +1,29 @@
+def badge_maker
+  "Arel"
+end
+puts "Hello, my name is #{"Arel"}."
+
+
+def badge_batch_creator(name)
+  counter = 0
+  while (counter < name.length)
+  puts "Hello, my name is #{name[counter]}."
+  counter += 1
+ end 
+end 
+  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+  
+  badge_batch_creator(name)
+  
+  def assign_rooms(name)
+    name.each_with_index |name,room|
+    counter=0
+      name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+      room = ["1","2","3","4","5","6","7"]
+    puts "Hello,#{name[counter]}! You'll be assigned to room #{[room]}!"
+    counter +=1
+end
+  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+  room = ["1","2","3","4","5","6","7"]
+    assign_rooms(room)
+    
