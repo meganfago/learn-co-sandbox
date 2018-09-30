ab187da2355e32a93e1df0fef7753b6043b68b94
benefic-dispatch-1249 <meganlaynefago@gmail.com>
2018-09-30 19:40:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,6 @@ end
     puts "Hello,#{name[counter]}! You'll be assigned to room #{room}!"
     counter +=1
   end
-end
   name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
     assign_rooms(name)
     
