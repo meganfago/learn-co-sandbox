247c32a0751cde18a38d66f668f45a9464ce4a26
benefic-dispatch-1249 <meganlaynefago@gmail.com>
2018-09-30 19:12:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,13 @@ def badge_maker
   "Arel"
 end
 puts "Hello, my name is #{"Arel"}."
+end
 
 def badge_batch_creator(name)
   counter = 0
   puts "Hello, my name is #{name}."
-  
+  counter += 1
+ end 
+end 
   name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+  badge_batch_creator(name)
